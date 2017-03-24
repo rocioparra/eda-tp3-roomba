b870d4ac190136ab6895f68e05c51e60028454c3
idiaz97 <idiaz@itba.edu.ar>
2017-03-24 16:00:35 -0300

    Agregue los point de roch a mi branch

---
+++ Point.cpp
@@ -0,0 +1,28 @@
+#include "Point.h"
+
+
+Point::Point(double _x, double _y)
+{
+	x = _x;	//inicializa ambas coordenadas con los valores recibidos (o 0 por defecto)
+	y = _y;
+}	
+
+double Point::getX()
+{
+	return x;
+}
+
+double Point::getY()
+{
+	return y;
+}
+
+void Point::setX(double _x)
+{
+	x = _x;
+}
+
+void Point::setY(double _y)
+{
+	y = _y;
+}
--- Point.h
+++ Point.h
@@ -1,6 +1,20 @@
+#ifndef POINT_H
+#define POINT_H
+
 class Point
 {
-public:
+  private:
 	double x;
 	double y;
+
+  public:
+	Point(double _x=0.0, double _y=0.0);	//punto por default = (0,0)
+
+	double getX();	//getters
+	double getY();
+
+	void setX(double _x);	//setters (cualquier valor es valido)
+	void setY(double _y);
 };
+
+#endif
