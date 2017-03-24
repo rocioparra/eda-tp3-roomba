57cf7a1f2a1e78c105ea00f8e70a924cfcd7d543
idiaz97 <idiaz@itba.edu.ar>
2017-03-24 17:04:47 -0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,28 +1,28 @@
 #include "Point.h"
 
 
-Point::Point(double _x, double _y)
+Point :: Point(double _x, double _y)
 {
 	x = _x;	//inicializa ambas coordenadas con los valores recibidos (o 0 por defecto)
 	y = _y;
 }	
 
-double Point::getX()
+double Point :: getX()
 {
 	return x;
 }
 
-double Point::getY()
+double Point :: getY()
 {
 	return y;
 }
 
-void Point::setX(double _x)
+void Point :: setX(double _x)
 {
 	x = _x;
 }
 
-void Point::setY(double _y)
+void Point :: setY(double _y)
 {
 	y = _y;
 }
