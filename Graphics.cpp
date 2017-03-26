2b2a5a237d8eade488fb49d065efde182f3f0552
malustewart <mastewart@itba.edu.ar>
2017-03-26 17:14:43 -0300

    graphic. falta calculo tamanio tiles y destructor
    Hay que linkear todas las librerias de allegro que estan en el Graphics.h

--- Graphics.cpp
+++ Graphics.cpp
@@ -10,6 +10,11 @@ Graphics::Graphics(uint xTiles, uint yTiles)
 //      fprintf(stderr, "failed to initialize primitives!\n");
 //	return -1;
     }
+    if(!al_init_image_addon()){
+    	al_shutdown_primitives_addon();
+        //printf("aejh");
+        //return -1;
+    }
     
     tileSide = TILESIDE;    //preg: ES MEGA AL PEDO ESTA VARIABLE NO?
     displayWidth = tileSide*xTiles;
@@ -21,8 +26,11 @@ Graphics::Graphics(uint xTiles, uint yTiles)
 //       fprintf(stderr, "failed to create display!\n");
 //       return -1;
     } 
+    al_clear_to_color(al_map_rgb(50,50,50));
     
-    robotBitmap = al_load_bitmap(ROBOT_SPRITE);
+    robotBitmap = al_load_bitmap("Travolta.png");
+    if(robotBitmap == NULL)
+        printf("no cargo bitmap\n");
 }
 Point Graphics::getTileCorner(uint i, uint j) //devuelve un punto en la esquina superior izquierda de la baldosa
 {
@@ -34,19 +42,42 @@ Point Graphics::getTileCorner(uint i, uint j) //devuelve un punto en la esquina 
 ALLEGRO_COLOR Graphics::getTileColor(Point tileCorner)
 {
     //da valores cualquiera a r, g y b.
-    unsigned char r = (unsigned char)(tileCorner.getY() * tileCorner.getY());
-    unsigned char g = (unsigned char)(tileCorner.getX() * tileCorner.getY());
-    unsigned char b = (unsigned char)(tileCorner.getX() * tileCorner.getX());
-    return(al_map_rgb(r,g,b));
+//    unsigned char r = (unsigned char)(tileCorner.getY() * tileCorner.getY());
+//    unsigned char g = (unsigned char)(tileCorner.getX() * tileCorner.getY());
+//    unsigned char b = (unsigned char)(tileCorner.getX() * tileCorner.getX());
+//    return(al_map_rgb(r,g,b));
+    return(al_map_rgb(255,255,255));
 }
 void Graphics::drawRobot(Point center, double angle)
 {
     //no esta bien invocada la funcion todavia
-//    al_draw_scaled_rotated_bitmap( *ROBOT_SPRITE, al_get_bitmap_width(ROBOT_SPRITE)/2, al_get_bitmap_height(ROBOT_SPRITE)/2, center.getX(), center.getY(), 0.5, 0.5, angle, 0);
+    al_draw_scaled_rotated_bitmap( robotBitmap, al_get_bitmap_width(robotBitmap)/2, al_get_bitmap_height(robotBitmap)/2, center.getX(), center.getY(), 0.5, 0.5, angle, 0);
 }
-void Graphics::drawFloor(bool* tiles)
-{
-    
+void Graphics::drawFloor(bool* tiles, int xTiles, int yTiles)//podria no recibir yTiles pero se lo incluye por claridad
+{   
+    for( int j = 0; j < yTiles ; j++ )
+    {
+        for( int i = 0; i < xTiles ; i++ )
+        {
+            Point corner = getTileCorner(i,j);
+            
+            if(tiles[ j*xTiles + i ] == true) //si esta limpio
+            {
+                //dibujar la baldosa de color
+                al_draw_filled_rectangle(corner.getX(), corner.getY(), corner.getX()+tileSide, corner.getY()+tileSide, getTileColor(corner) );        
+                //dibujar un contorno negro alrededor de la baldoza
+                al_draw_rectangle(corner.getX(), corner.getY(), corner.getX()+tileSide, corner.getY()+tileSide, al_map_rgb(0,0,0), 0 );        
+            }
+            else
+            {
+                //dibujar la baldoza de negro
+                al_draw_filled_rectangle(corner.getX(), corner.getY(), corner.getX()+tileSide, corner.getY()+tileSide, al_map_rgb(0,0,0) );        
+                //dibujar un contorno negro alrededor de la baldoza
+                al_draw_rectangle(corner.getX(), corner.getY(), corner.getX()+tileSide, corner.getY()+tileSide, al_map_rgb(255,255,255), 0 );        
+
+            }
+        }
+    }
 }
 void Graphics::showChanges()
 {
--- Graphics.h
+++ Graphics.h
@@ -1,11 +1,13 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <stdio.h>
 #include "Floor.h"
 #include "Robot.h"
 #include "Simulation.h"
 #include "Point.h"
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h> 
 #include <allegro5/allegro_color.h>
 #include <allegro5/allegro_primitives.h>
 
@@ -17,18 +19,18 @@ typedef unsigned int uint;
 class Graphics
 {
 private:
-    ALLEGRO_DISPLAY* display;    
-    ALLEGRO_BITMAP* robotBitmap;
+    ALLEGRO_DISPLAY* display = NULL;    
     uint tileSide;              //lado de una baldosa (son cuadradas)
     uint displayHeight;
     uint displayWidth;
-    Point getTileCorner(uint i, uint j);  //recibe fila y columna de la baldosa, devuelve el punto de la esquina superior derecha
-    ALLEGRO_COLOR getTileColor(Point tileCorner); //recibe la esquina de la tile y devuelve un color
+    Point getTileCorner(uint i, uint j);            //recibe fila y columna de la baldosa, devuelve el punto de la esquina superior derecha
+    ALLEGRO_COLOR getTileColor(Point tileCorner);   //recibe la esquina de la tile y devuelve un color
 
 public:
-    Graphics(uint xTiles, uint yTiles);         //inicializa allegro y calcula el tamanio de las baldosas
+    ALLEGRO_BITMAP* robotBitmap = NULL;
+    Graphics(uint xTiles, uint yTiles);          //inicializa allegro y calcula el tamanio de las baldosas
     void drawRobot(Point center, double angle);  //dibuja un robot en el punto center con el angulo angle
-    void drawFloor(bool* tiles);                 //dibuja el piso con los colores correspondientes a sucio y limpio
+    void drawFloor(bool* tiles, int i, int j);   //dibuja el piso con los colores correspondientes a sucio y limpio
     void showChanges();                          //muestra lo dibujado en pantalla
 };
 #endif /* GRAPHICS_H */
