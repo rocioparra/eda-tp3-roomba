045fe26d2e592cb920ec5cb2ad253099a6537250
malustewart <mastewart@itba.edu.ar>
2017-03-26 19:48:45 -0300

    Add files via upload

--- Graphics.h
+++ Graphics.h
@@ -18,21 +18,20 @@ typedef unsigned int uint;
 
 class Graphics
 {
-
 private:
-    ALLEGRO_DISPLAY* display = NULL;    
+    ALLEGRO_DISPLAY* display;    
     uint tileSide;              //lado de una baldosa (son cuadradas)
     uint displayHeight;
     uint displayWidth;
     Point getTileCorner(uint i, uint j);            //recibe fila y columna de la baldosa, devuelve el punto de la esquina superior derecha
     ALLEGRO_COLOR getTileColor(Point tileCorner);   //recibe la esquina de la tile y devuelve un color
 
-
 public:
-    ALLEGRO_BITMAP* robotBitmap = NULL;
+    ALLEGRO_BITMAP* robotBitmap;
     Graphics(uint xTiles, uint yTiles);          //inicializa allegro y calcula el tamanio de las baldosas
-    void drawRobot(Point center, double angle);  //dibuja un robot en el punto center con el angulo angle
+    void drawRobot(uint xCenter, uint yCenter, double angle);  //dibuja un robot en el punto center con el angulo angle
     void drawFloor(bool* tiles, int i, int j);   //dibuja el piso con los colores correspondientes a sucio y limpio
     void showChanges();                          //muestra lo dibujado en pantalla
 };
 #endif /* GRAPHICS_H */
+    
