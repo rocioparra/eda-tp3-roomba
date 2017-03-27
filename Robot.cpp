6fb903cfbf43c02b601264fbce1752369bf428d8
idiaz97 <idiaz@itba.edu.ar>
2017-03-27 03:45:55 +0200

    "Arregle "la funcion checkAngle

--- Robot.cpp
+++ Robot.cpp
@@ -81,42 +81,18 @@ Point Robot::randomPoint(void)		//Genera un punto random del plano
 
 bool Robot::checkAngle(void)  //Funcion que genera un nuevo angulo SOLO EN EL CASO DE SER NECESARIO, en caso de no serlo, deja el angulo como estaba.
 {
-	double _angle = changeAngle2Rad();
-	double c= p.getX() + sin(_angle);
-	double d = p.getY() + cos(_angle);
-	bool changed = true;
-
-	if (c < 0 && d < 0)
+	double nextX;
+	double nextY;
+	double _angle;
+	_angle = changeAngle2Rad();
+	while (((nextX = p.getX()+sin(_angle)) > xmax) || (nextX<0) || ((nextY = p.getY()+cos(_angle)) > ymax) || (nextY<0))
 	{
-		angle = (rand() % 90) + 90;	//darle un angulo que seguro sirva
+		angle = (rand() % 360);
+		_angle = changeAngle2Rad();
 	}
-	else if ( c>xmax && d>ymax )
-	{
-		angle = (rand() % 90) + 270;
-	}
-	else if ( c<0 )
-	{
-		angle = (rand() % 180) + 90;
-	}
-	else if (d<0)
-	{
-		angle = (rand() % 180);	
-	}
-	else if (c>xmax)
-	{
-		angle = (rand() % 180) - 90;
-		angle = (angle >= 0) ? angle : (360 + angle);
-	}
-	else if (d>ymax)
-	{
-		angle = (rand() % 180) + 180;
-	}
-	else
-	{
-		changed = false;
-	}
-
-	return changed;
+	if((nextX= p.getX()+sin(_angle)<xmax) && (nextX>0) && (nextY=p.getY()+cos(_angle)<ymax) && (nextY>0))
+		return false;
+	return true;
 }
 
 bool Robot::checkEverythingOk(void)
