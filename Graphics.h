81289b73bf436195d5905a2d97bda46bef561ff5
malustewart <mastewart@itba.edu.ar>
2017-03-29 17:06:16 -0300

    cambio de tabs de 8 a 4

--- Graphics.h
+++ Graphics.h
@@ -20,23 +20,23 @@
 class Graphics
 {
 private:
-    ALLEGRO_DISPLAY* display;    
-    uint32_t tileSide;              //lado de una baldosa (son cuadradas)
-    uint32_t displayHeight;
-    uint32_t displayWidth;
-    bool valid;     //si hubieron errores en el constructor, es false.
-    float resizingFactor;   //valor utilizado para ajustar el tamanio del sprite del robot
-    Point getTileCorner(uint32_t i, uint32_t j);            //recibe fila y columna de la baldosa, devuelve el punto de la esquina superior derecha
-    ALLEGRO_COLOR getTileColor(Point tileCorner);   //recibe la esquina de la tile y devuelve un color
+    	ALLEGRO_DISPLAY* display;    
+    	uint32_t tileSide;              //lado de una baldosa (son cuadradas)
+    	uint32_t displayHeight;
+    	uint32_t displayWidth;
+    	bool valid;     //si hubieron errores en el constructor, es false.
+    	float resizingFactor;   //valor utilizado para ajustar el tamanio del sprite del robot
+    	Point getTileCorner(uint32_t i, uint32_t j);            //recibe fila y columna de la baldosa, devuelve el punto de la esquina superior derecha
+    	ALLEGRO_COLOR getTileColor(Point tileCorner);   //recibe la esquina de la tile y devuelve un color
 
 public:
-    ALLEGRO_BITMAP* robotBitmap;
-    Graphics(uint32_t xTiles, uint32_t yTiles);          //inicializa allegro y calcula el tamanio de las baldosas
-    void drawRobot(float xCenter, float yCenter, double angle);  //dibuja un robot en el punto center con el angulo angle
-    void drawFloor(bool* tiles, uint32_t i, uint32_t j);   //dibuja el piso con los colores correspondientes a sucio y limpio
-    void showChanges();                          //muestra lo dibujado en pantalla
-    void showTickCount(uint32_t);
-    bool isValid();
-    void destructor();
+    	ALLEGRO_BITMAP* robotBitmap;
+    	Graphics(uint32_t xTiles, uint32_t yTiles);          //inicializa allegro y calcula el tamanio de las baldosas
+    	void drawRobot(float xCenter, float yCenter, double angle);  //dibuja un robot en el punto center con el angulo angle
+    	void drawFloor(bool* tiles, uint32_t i, uint32_t j);   //dibuja el piso con los colores correspondientes a sucio y limpio
+    	void showChanges();                          //muestra lo dibujado en pantalla
+    	void showTickCount(uint32_t);
+    	bool isValid();
+    	void destructor();
 };
 #endif /* GRAPHICS_H */
