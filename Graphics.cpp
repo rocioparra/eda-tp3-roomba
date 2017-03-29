b784b3eaaa8d27212902f4316bedf44d484def38
malustewart <mastewart@itba.edu.ar>
2017-03-29 18:34:02 -0300

    agregue cometarios

--- Graphics.cpp
+++ Graphics.cpp
@@ -6,7 +6,9 @@ using namespace std;
 Graphics::Graphics(uint32_t xTiles, uint32_t yTiles)
 {
 	valid = true;
-	if(!al_init())
+	//se inicializan las librerias de allegro. En caso de que haya un error, se indica 
+	//en la variable valid y se llaman a las funciones correspondientes para la finalizacion
+	if(!al_init())		
 		valid = false;
 	else if(!al_init_primitives_addon())
 		valid = false;
@@ -16,7 +18,7 @@ Graphics::Graphics(uint32_t xTiles, uint32_t yTiles)
         	valid = false;
     	}
 	if (valid)
-        	robotBitmap = al_load_bitmap("Travolta.png");
+        	robotBitmap = al_load_bitmap("Travolta.png");	//bitmap de los Robots
     	if(robotBitmap == nullptr)
     	{
         	al_shutdown_primitives_addon();
@@ -26,7 +28,7 @@ Graphics::Graphics(uint32_t xTiles, uint32_t yTiles)
     
     	float screenResX = 1600, screenResY = 900;  //falta averiguar la resolucion del display en ejecucion  
     
-    	//tamanio de balosa: minimo entre ancho de pantalla/xTiles, alto de pantalla/ytiles, y el maximo establecido en MAXTILESIDE
+    	//tamanio de baldosa: minimo entre ancho de pantalla/xTiles, alto de pantalla/ytiles, y el maximo establecido en MAXTILESIDE
     	tileSide = min( min((uint32_t)(screenResX/xTiles),(uint32_t)(screenResY/yTiles)) , MAXTILESIDE);
 
     	//constante por la cual hay que modificar el tamanio del sprite del robot en relacion al tamanio de las baldosas
@@ -62,7 +64,7 @@ void Graphics::drawFloor(bool* tiles, uint32_t xTiles, uint32_t yTiles)//podria 
         	}
     	}
 }
-void Graphics::drawRobot(float xCenter, float yCenter, double angle)
+void Graphics::drawRobot(float xCenter, float yCenter, double angle) //dibujar el robot en el angulo indicado
 {
     	al_draw_scaled_rotated_bitmap( robotBitmap, al_get_bitmap_width(robotBitmap)/2, al_get_bitmap_height(robotBitmap)/2, xCenter*tileSide, yCenter*tileSide, resizingFactor, resizingFactor, angle, 0);
 }
@@ -88,10 +90,10 @@ ALLEGRO_COLOR Graphics::getTileColor(Point tileCorner)
 }
 void Graphics::showTickCount(uint32_t tickCount)
 {
-    	char tickCountA[100];       //arreglo para uardar el codigo ascii del tickCount
+    	char tickCountA[100];       	//arreglo para uardar el codigo ascii del tickCount
 //    	snprintf( tickCountA, sizeof(tickCountA), "%d", tickCount );    //generar el string de tickCount
     
-    	al_show_native_message_box(
+    	al_show_native_message_box(	//indicar al usuario el tickCount
     	NULL,
     	"Fiebre de sabado por la noche",
     	"Tu fiebre de sabado por la noche llego a los siguientes grados:",
