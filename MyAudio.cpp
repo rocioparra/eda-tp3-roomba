441c9b3a05dfe1258db1f3c045e2f55332121993
malustewart <mastewart@itba.edu.ar>
2017-03-29 17:08:33 -0300

    cambio tabs de 4 a 8

--- MyAudio.cpp
+++ MyAudio.cpp
@@ -2,61 +2,57 @@
 
 MyAudio::MyAudio(uint samplesReservation)
 {
-    valid = true;
-    if(!al_init())
-    {
-        valid = false;
-    }
-    else if(!al_install_audio())
-    {
-        valid = false;
-    }
-    else if(!al_init_acodec_addon())
-    {
-        al_uninstall_audio();
-        valid = false;
-    }
-    else if( !al_reserve_samples(samplesReservation))
-    {
-            al_uninstall_audio();
-            valid = false;
-    }
-    samplesStored = 0;
+   	valid = true;
+   	if(!al_init())
+        	valid = false;
+    	else if(!al_install_audio())
+    	    	valid = false;
+    	else if(!al_init_acodec_addon())
+    	{
+        	al_uninstall_audio();
+        	valid = false;
+    	}
+    	else if( !al_reserve_samples(samplesReservation))
+    	{
+            	al_uninstall_audio();
+            	valid = false;
+    	}
+    	samplesStored = 0;
 }
 sampleID MyAudio::loadSample(char* fileName)
 {
-    sampleID newSample= al_load_sample(fileName);   //cargar el archivo del sample en memoria
-    if(newSample)
-        loadedSamples[samplesStored++] = newSample;//si se cargo exitosamente, guardar el ID en la primer 
+    	sampleID newSample= al_load_sample(fileName);   //cargar el archivo del sample en memoria
+    	if(newSample)
+        	loadedSamples[samplesStored++] = newSample;//si se cargo exitosamente, guardar el ID en la primer 
                                                     //posicion libre del arreglo y "apuntar" al siguiente 
                                                     //lugar libre del arreglo aumentando samplesStored
-    return newSample;   //si hubo error, devuelve NULL, sino devuelve su ID
+    	return newSample;   //si hubo error, devuelve NULL, sino devuelve su ID
 }
 void MyAudio::playSampleOnce(sampleID sample)
 {
-    al_play_sample(sample, 1.0, ALLEGRO_AUDIO_PAN_NONE, 1.0, ALLEGRO_PLAYMODE_ONCE, nullptr);
+    	al_play_sample(sample, 1.0, ALLEGRO_AUDIO_PAN_NONE, 1.0, ALLEGRO_PLAYMODE_ONCE, nullptr);
 }
 void MyAudio::playSampleLooped(ALLEGRO_SAMPLE* sample)
 {
-    al_play_sample(sample, 1.0, ALLEGRO_AUDIO_PAN_NONE, 1.0, ALLEGRO_PLAYMODE_LOOP, nullptr);
+    	al_play_sample(sample, 1.0, ALLEGRO_AUDIO_PAN_NONE, 1.0, ALLEGRO_PLAYMODE_LOOP, nullptr);
 }
 void MyAudio::stopSample(ALLEGRO_SAMPLE* sample)
 {
-    al_stop_sample((ALLEGRO_SAMPLE_ID*)sample);
+    	al_stop_sample((ALLEGRO_SAMPLE_ID*)sample);
 }
 void MyAudio::stopSamples()
 {
-    al_stop_samples();
+    	al_stop_samples();
 }
 bool MyAudio::isValid()
 {
-    return valid;
+    	return valid;
 }
 void MyAudio::destructor()
 {
-    for(int i = 0; i < samplesStored; ++i)
-    {
-        al_destroy_sample(loadedSamples[i]);
-    }
-    al_uninstall_audio();
+    	for(int i = 0; i < samplesStored; ++i)
+    	{
+        	al_destroy_sample(loadedSamples[i]);
+    	}
+   	al_uninstall_audio();
 }
