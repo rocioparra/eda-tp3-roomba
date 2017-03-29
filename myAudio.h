1be247d3a472de88849d89aa017fa071de44422b
malustewart <mastewart@itba.edu.ar>
2017-03-29 17:16:56 -0300

    cambio tabs de 4 a 8

--- myAudio.h
+++ myAudio.h
@@ -13,18 +13,18 @@ typedef ALLEGRO_SAMPLE* sampleID;   //sirve para tener un mayor nivel de abstrac
 
 class MyAudio{
 private:
-    sampleID loadedSamples[MAX_SAMPLES];       //Se agrega por escalabilidad y para destruir los samples al finalizar el programa
-    uint samplesStored;
-    bool valid;
+	sampleID loadedSamples[MAX_SAMPLES];       //Se agrega por escalabilidad y para destruir los samples al finalizar el programa
+	uint samplesStored;
+	bool valid;
 public:
-    MyAudio(uint sampleOverlap);                //sampleOverlap: hasta cuantas samples se pueden escuchar al mismo tiempo
-    sampleID loadSample(char* fileName);        //carga en memoria el sample deseado 
-    void playSampleOnce(sampleID sample);       //Reproduce una vez el sample recibido
-    void playSampleLooped(sampleID sample);     //Reproduce loopeado el sample recibido
-    void stopSample(sampleID sample);           //detiene la reproduccion del sample recibido
-    void stopSamples();                         //detiene la reproduccion de todas las samples. No es necesario en este programa pero se agrega por escalabilidad 
-    bool isValid();
-    void destructor();
+	MyAudio(uint sampleOverlap);                //sampleOverlap: hasta cuantas samples se pueden escuchar al mismo tiempo
+	sampleID loadSample(char* fileName);        //carga en memoria el sample deseado 
+	void playSampleOnce(sampleID sample);       //Reproduce una vez el sample recibido
+	void playSampleLooped(sampleID sample);     //Reproduce loopeado el sample recibido
+	void stopSample(sampleID sample);           //detiene la reproduccion del sample recibido
+	void stopSamples();                         //detiene la reproduccion de todas las samples. No es necesario en este programa pero se agrega por escalabilidad 
+	bool isValid();
+	void destructor();
 };
 
 #endif /* MYAUDIO_H */
