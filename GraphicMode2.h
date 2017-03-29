ebaf475085d5d929d254c1bcc6da6287d8cda54f
malustewart <mastewart@itba.edu.ar>
2017-03-29 18:24:48 -0300

    agregue comentarios

--- GraphicMode2.h
+++ GraphicMode2.h
@@ -1,7 +1,7 @@
 #ifndef GRAPHICMODE2_H
 #define GRAPHICMODE2_H
 
-#include <stdio.h>      //ACA HAY BOCHA DE INCLUDES AL PEDO 
+#include <stdio.h>
 #include "Floor.h"
 #include "Robot.h"
 #include "Simulation.h"
@@ -13,27 +13,27 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 
-#define FONTSIZE 20
+#define FONTSIZE 20     //tamaño de letra usada para etiquetar los ejes y el maximo valor
 
-class GraphicMode2
+class GraphicMode2 //genera un grafico de barras 
 {
 private:
-        ALLEGRO_DISPLAY* display;   
-        ALLEGRO_FONT* defaultFont;    
-        Point origin;     //coordenada de origen del grafico
-        uint32_t barWidth;
-        uint32_t barAmount;
+        ALLEGRO_DISPLAY* display;
+        ALLEGRO_FONT* defaultFont;
+        Point origin;                   //coordenada de origen del grafico
+        uint32_t barWidth; 
+        uint32_t barAmount;             
         uint32_t displayHeight;
         uint32_t displayWidth;
-        float resizingFactor;
-        bool valid;
-        void drawBar(uint32_t barPosition, uint32_t barHeight);
+        float resizingFactor;           //modificacion de tamñano de las barras para que no se excedan del display
+        bool valid;                     //true: no hubo error. false: hubo error.
+        void drawBar(uint32_t barPosition, uint32_t barHeight); //dibuja una barra en el grafico
 
 public:
         GraphicMode2(uint32_t simRepetition); //inicializa allegro y calcula el tamanio de las barras
         void drawAllBars(double barValue[]); //dibuja todas las barras del grafico
         void showChanges(); //muestra lo dibujado en pantalla
-        bool isValid();
+        bool isValid(); //indica si hubo error o no en el constructor
 };
 
 #endif /* GRAPHICMODE2_H */
