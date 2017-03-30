c28e554a22e3ad7a04fcc39f79d91a27f65fe977
rocioparra <rparra@itba.edu.ar>
2017-03-30 14:51:12 -0300

    para entregar: point

--- Point.h
+++ Point.h
@@ -1,5 +1,27 @@
 #ifndef POINT_H
 #define POINT_H
+
+/*	
+
+	Instituto Tecnológico de Buenos Aires
+	22.08 - Algoritmos y estructura de datos
+
+	Trabajo práctico n° 3: Robots
+
+	Autores:	Díaz, Ian Cruz				- legajo 57.515
+				Parra, Rocío				- legajo 57.669
+				Stewart Harris, María Luz	- legajo 
+
+	Fecha de entrega: jueves 30 de marzo de 2017
+
+***********************************************************************************************************
+	
+	Clase Point
+	Cada elemento contiene dos coordenadas, que son de tipo double.
+
+*/
+
+
 #include <cstdint>
 
 class Point
